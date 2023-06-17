88144363e92b02dc1890499b2e2f15cf4c03f06d
kosmos4v <kosmos4v@gmail.com>
2023-06-17 20:59:10 +0300

    feat: creat router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Author from './pages/Author';
+import User from './pages/User';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      Привет
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/author" element={<Author />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </Router>
   );
 }
 
