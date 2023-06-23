964ab13f33803f941ff7275544cb8e2a84d7bf8c
kosmos4v <kosmos4v@gmail.com>
2023-06-23 13:31:12 +0300

    feat: add redirect to user page by click on the image

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -5,23 +5,29 @@ type PostProps = {
   title: string,
   text: string,
   buttonText: string,
+  onClick?: () => void,
 };
 
-export const Post: React.FC<PostProps> = ({ title, text, buttonText }) => {
-  const a = 5;
-
-  return (
-    <div>
-      <Card className="mb-2">
-        <Container style={{ display: 'flex', flexDirection: 'row' }}>
-          <Card.Img variant="left" src="/images/avatar.jpg" className="post__img mt-3" style={{ width: '100px', height: '100px' }} />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{text}</Card.Text>
-            <Button variant="link">{buttonText}</Button>
-          </Card.Body>
-        </Container>
-      </Card>
-    </div>
-  );
-};
+export const Post: React.FC<PostProps> = ({
+  title,
+  text,
+  buttonText,
+  onClick,
+}) => (
+  <Card className="mb-2">
+    <Container style={{ display: 'flex', flexDirection: 'row' }}>
+      <Card.Img
+        variant="left"
+        src="/images/avatar.jpg"
+        className="post__img mt-3"
+        style={{ width: '100px', height: '100px', cursor: 'pointer' }}
+        onClick={onClick}
+      />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+        <Button variant="link">{buttonText}</Button>
+      </Card.Body>
+    </Container>
+  </Card>
+);
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { TRootState } from '../../redux/reducers';
 import { loadPosts } from '../../redux/actions/post';
 import Post from '../../components/Post';
 
 export const Main: React.FC = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleClickCard = useCallback(() => {
+    navigate('/user');
+  }, [navigate]);
   const { posts } = useSelector((state: TRootState) => state.post);
-  useEffect(() => {
-    dispatch(loadPosts());
-  }, [dispatch]);
-  console.log(posts);
+  useEffect(() => { dispatch(loadPosts()); }, [dispatch]);
   return (
     <div>
       {posts?.map((post) => (
@@ -19,6 +21,7 @@ export const Main: React.FC = () => {
           title={post.title}
           text={post.text}
           buttonText="Комментарии"
+          onClick={handleClickCard}
         />
       ))}
     </div>
